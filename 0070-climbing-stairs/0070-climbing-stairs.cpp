43bdb25ec784553479ab0437a15637ea009cf3d6
Ashutosh Maurya <aashutoshmaurya585@gmail.com>
2025-03-25 16:41:33 +0530

    Time: 0 ms (100%), Space: 8.6 MB (33.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
+vector<int>dp;
+    int ways(int i,int n){
+        if(i==n) return 1;
+        if(i==n+1) return 0;
+
+        if(dp[i]!=-1) return dp[i];
+        return dp[i]=ways(i+1,n)+ways(i+2,n);
+    }
+
     int climbStairs(int n) {
-        int a=0,b=1,c=0;
-        for(int i=n-1;i>=0;i--){
-            c=a+b;
-            a=b;
-            b=c;
-        }
-        return c;
-        
+        dp.resize(n,-1);
+        return ways(0,n);
     }
 };
