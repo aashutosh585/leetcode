9f1fd6ba61fe563fe05730974cf2ba037ebaeec9
Ashutosh Maurya <aashutoshmaurya585@gmail.com>
2025-03-31 20:11:46 +0530

    Time: 122 ms (56.2%), Space: 17 MB (62.37%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    // vector<vector<bool>>dp;
+    vector<vector<bool>>dp;
     // bool solve(int n,int sum,vector<int>&arr){
     //     if(sum==0) return true;
     //     if(n==0) return false;
@@ -13,33 +13,52 @@ public:
     // }
     bool canPartition(vector<int>& nums) {
 
-        int sum=accumulate(nums.begin(),nums.end(),0);
+        int n=nums.size(),sum=accumulate(nums.begin(),nums.end(),0);
         if(sum%2!=0) return false;
 
         sum=sum/2;
         // dp.resize(nums.size()+1,vector<bool>(sum+1,false));
         // return solve(nums.size(),sum,nums);
 
-        //  bottom up
+        //  bottom up approch
+        dp.resize(nums.size()+1,vector<bool>(sum+1,false));
 
+        for(int i=0;i<n;i++){
+            dp[i][0]=true;
+        }
+        
 
-
-
-        int target = sum ;
-        vector<bool> dp(target + 1, false);
-        dp[0] = true;
-
-
-
-         for (int n : nums) {
-            for (int i = target; i >= n; i--) {
-                if (dp[i]) continue;
-                if (dp[i - n]) dp[i] = true;
-                if (dp[target]) return true;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum;j++){
+                if(j<nums[i-1]){
+                    dp[i][j]=dp[i-1][j];
+                }
+                else{
+                    dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                }
             }
         }
+        return dp[n][sum];
 
-        return false;  
+
+
+
+
+        // int target = sum ;
+        // vector<bool> dp(target + 1, false);
+        // dp[0] = true;
+
+
+
+        //  for (int n : nums) {
+        //     for (int i = target; i >= n; i--) {
+        //         if (dp[i]) continue;
+        //         if (dp[i - n]) dp[i] = true;
+        //         if (dp[target]) return true;
+        //     }
+        // }
+
+        // return false;  
 
 
 
