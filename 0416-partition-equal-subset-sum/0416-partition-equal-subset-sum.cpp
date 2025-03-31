9b718398e5473d70d6bd9b5b73830fa65a14d755
Ashutosh Maurya <aashutoshmaurya585@gmail.com>
2025-03-31 20:44:44 +0530

    Time: 66 ms (85.85%), Space: 13.3 MB (91.17%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<vector<bool>>dp;
+    // vector<vector<bool>>dp;
     // bool solve(int n,int sum,vector<int>&arr){
     //     if(sum==0) return true;
     //     if(n==0) return false;
@@ -21,24 +21,36 @@ public:
         // return solve(nums.size(),sum,nums);
 
         //  bottom up approch
-        dp.resize(nums.size()+1,vector<bool>(sum+1,false));
+        // dp.resize(nums.size()+1,vector<bool>(sum+1,false));
 
-        for(int i=0;i<n;i++){
-            dp[i][0]=true;
-        }
+        // for(int i=0;i<n;i++){
+        //     dp[i][0]=true;
+        // }
         
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=sum;j++){
-                if(j<nums[i-1]){
-                    dp[i][j]=dp[i-1][j];
-                }
-                else{
-                    dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                }
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=sum;j++){
+        //         if(j<nums[i-1]){
+        //             dp[i][j]=dp[i-1][j];
+        //         }
+        //         else{
+        //             dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
+        //         }
+        //     }
+        // }
+        // return dp[n][sum];
+
+
+        // space optimize
+        vector<bool>dp(sum+1,false);
+
+        dp[0]=true;
+        for(int i=0;i<n;i++){
+            for(int j=sum;j>=nums[i];j--){
+                dp[j] = dp[j] || dp[j - nums[i]];
             }
         }
-        return dp[n][sum];
+        return dp[sum];
 
 
 
