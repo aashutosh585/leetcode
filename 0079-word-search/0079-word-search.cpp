e8a2777dec7dc43f222b6d725e0830aac03801bf
Ashutosh Maurya <183261112+aashutosh585@users.noreply.github.com>
2025-08-21 09:41:23 +0530

    Time: 195 ms (80.15%), Space: 10.5 MB (86.23%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,36 +1,35 @@
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        
-        function<bool(int, int, int)> backtrack = [&](int i, int j, int k) {
-            if (k == word.length()) {
-                return true;
-            }
-            if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[k]) {
-                return false;
-            }
-            
-            char temp = board[i][j];
-            board[i][j] = '\0';
-            
-            if (backtrack(i + 1, j, k + 1) || backtrack(i - 1, j, k + 1) || 
-                backtrack(i, j + 1, k + 1) || backtrack(i, j - 1, k + 1)) {
-                return true;
-            }
-            
-            board[i][j] = temp; 
+  
+    bool solve(int idx,int i,int j,vector<vector<char>>& board, string &word){
+
+        if(idx==word.size()) return true;
+
+        if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j] != word[idx])
             return false;
-        };
         
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (backtrack(i, j, 0)) {
-                    return true;
+        char s=board[i][j];
+        board[i][j]='@';
+
+
+        if(solve(idx+1,i+1,j,board,word) || solve(idx+1,i-1,j,board,word) || solve(idx+1,i,j+1,board,word) || solve(idx+1,i,j-1,board,word)) return true;
+
+
+        board[i][j]=s;
+
+        return  false;
+
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j]==word[0]){
+                    if(solve(0,i,j,board,word)) return true;
                 }
             }
         }
+
         return false;
     }
 };
