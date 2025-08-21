b238a2092e72d54e095c3cac36fb3252e9df6f7e
Ashutosh Maurya <183261112+aashutosh585@users.noreply.github.com>
2025-08-21 13:03:10 +0530

    Time: 49 ms (6.01%), Space: 31.3 MB (9.67%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,43 +1,48 @@
-
 class Solution {
 public:
     vector<vector<string>> ans;
 
     void fill(vector<vector<bool>>& allowed, int i, int j) {
         int n = allowed.size();
-
-        //  mark col
-        for (int r = 0; r < n; r++) allowed[r][j] = true;
-
-        // diagonal ↙ and ↗
-        for (int x=i, y=j; x>=0 && y>=0; x--, y--) allowed[x][y] = true;
-        for (int x=i, y=j; x<n && y<n; x++, y++) allowed[x][y] = true;
-
-        // diagonal ↘ and ↖
-        for (int x=i, y=j; x>=0 && y<n; x--, y++) allowed[x][y] = true;
-        for (int x=i, y=j; x<n && y>=0; x++, y--) allowed[x][y] = true;
+        int x=i, y=j;
+        while (x>=0 && y>=0) allowed[x--][y--] = true;
+        x=i, y=j;
+        while (x<n && y<n) allowed[x++][y++] = true;
+        x=i, y=j;
+        while (y>=0) allowed[x][y--] = true;
+        x=i, y=j;
+        while (y<n) allowed[x][y++] = true;
+        x=i, y=j;
+        while (x>=0) allowed[x--][j] = true;
+        x=i, y=j;
+        while (x<n) allowed[x++][j] = true;
+        x=i, y=j;
+        while (x>=0 && y<n) allowed[x--][y++] = true;
+        x=i, y=j;
+        while (x<n && y>=0) allowed[x++][y--] = true;
     }
 
-    void solve(int queen, int row, vector<vector<bool>> allowed, vector<string>& arr, string s) {
+    void solve(int queen, int row, vector<vector<bool>> allowed,vector<string>&arr, string s) {
         int n = allowed.size();
         if (queen == 0) {
+            // reverse(arr.begin(),arr.end());
             ans.push_back(arr);
             return;
         }
         if (row >= n) return;
 
-        for (int col = 0; col < n; col++) {
+        for (int col=0; col<n; col++) {
             if (!allowed[row][col]) {
-                auto newAllowed = allowed;
+                auto newAllowed=allowed;
                 fill(newAllowed, row, col);
 
                 string a = s;
                 a[col] = 'Q';
                 arr.push_back(a);
 
-                solve(queen - 1, row + 1, newAllowed, arr, s);
-
-                arr.pop_back(); 
+                solve(queen-1, row+1, newAllowed, arr, s);
+                arr.pop_back();
+                
             }
         }
     }
@@ -45,19 +50,19 @@ public:
     vector<vector<string>> solveNQueens(int n) {
         string s(n, '.');
 
-        for (int i = 0; i < n; i++) {
+        for(int i=0;i<n;i++){
             vector<vector<bool>> allowed(n, vector<bool>(n, false));
             vector<string> arr;
 
-            fill(allowed, 0, i);
+            fill(allowed,0,i);
             string a = s;
             a[i] = 'Q';
             arr.push_back(a);
 
-            solve(n - 1, 1, allowed, arr, s);
+            solve(n-1, 1, allowed, arr, s);
+
         }
 
         return ans;
     }
 };
-
