4e4883ad5fa12c56bd340800ca35926bb79a5422
Ashutosh Maurya <183261112+aashutosh585@users.noreply.github.com>
2025-09-01 19:12:14 +0530

    Time: 368 ms (29.82%), Space: 109.1 MB (16.67%) - LeetHub

---
+++ 1917-maximum-average-pass-ratio/1917-maximum-average-pass-ratio.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    double gain(int x,int y){
+        double gain = (double)(x+1)/(y+1) -(double)x/y;
+        return gain; 
+    }
+
+    double maxAverageRatio(vector<vector<int>>& arr, int extra) {
+        priority_queue<pair<double, pair<int, int>>> pq;
+        for(auto it : arr){
+            pq.push({gain(it[0],it[1]),{it[0],it[1]}});
+        }
+
+        while(extra--){
+            auto [_, x]=pq.top(); pq.pop();
+            int pass = x.first+1;
+            int total = x.second+1;
+            pq.push({gain(pass,total),{pass,total}});
+        }
+
+        double sum=0;
+        while(!pq.empty()){
+            auto [_,p] =pq.top(); pq.pop();
+            sum+=(double)p.first/p.second;
+        }
+
+        sum=sum/arr.size();
+
+        return sum;
+
+        
+    }
+};
