10749265f8033f07bea1447a4d452b59d714a07d
Ashutosh Maurya <183261112+aashutosh585@users.noreply.github.com>
2025-09-25 20:16:04 +0530

    Time: 1 ms (20.26%), Space: 13.2 MB (60.92%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,12 +3,9 @@ public:
     vector<vector<int>>dp;
     int solve(int i,int l,vector<vector<int>>&arr){
         int n=arr.size();
-        if(l>=n) return 0;
-        if(i>=arr[l].size()) return INT_MAX;
-
-
-
-        // int x=arr[l][i]+min(solve(i,l+1,arr),solve(i+1,l+1,arr));
+        if(l==n) return 0; 
+        
+        if(i==arr[l].size()) return 0;
 
         if(dp[l][i]!=-1) return dp[l][i];
         int way1 = arr[l][i] + solve(i,l+1,arr);
