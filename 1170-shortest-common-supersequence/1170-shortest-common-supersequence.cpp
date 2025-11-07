829283db45ee81dd5fba09991e78fd56fa3983cb
Ashutosh Maurya <183261112+aashutosh585@users.noreply.github.com>
2025-11-07 23:35:03 +0530

    Time: 23 ms (87.96%), Space: 26.7 MB (69.9%) - LeetHub

---
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    
+
+    // string solve(int i, int j, const string &s1, const string &s2) {
+    //     if(i == (int)s1.size()) return s2.substr(j);
+    //     if(j == (int)s2.size()) return s1.substr(i);
+    //     if(!dp[i][j].empty()) return dp[i][j];
+
+    //     if(s1[i] == s2[j]) {
+    //         return dp[i][j] = s1[i] + solve(i + 1, j + 1, s1, s2);
+    //     } else {
+    //         string take1 = s1[i] + solve(i + 1, j, s1, s2);
+    //         string take2 = s2[j] + solve(i, j + 1, s1, s2);
+    //         return dp[i][j] = (take1.size() <= take2.size() ? take1 : take2);
+    //     }
+    // }
+
+    string shortestCommonSupersequence(string s1, string s2) {
+        int n = s1.size(), m = s2.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1,0)); 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1[i - 1] == s2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        string res="";
+        int i=n,j=m;
+        while(i>0 && j>0){
+            if(s1[i-1]==s2[j-1]){
+                res+=s1[i-1];
+                i--,j--;
+            }
+            else if(dp[i-1][j]>dp[i][j-1]){
+                res+=s1[i-1];
+                i--;
+            }
+            else{
+                res+=s2[j-1];
+                j--;
+            }
+        }
+
+        while (i > 0) res += s1[i - 1], i--;
+        while (j > 0) res += s2[j - 1], j--;
+
+        reverse(res.begin(), res.end());
+        return res;
+    }
+};
