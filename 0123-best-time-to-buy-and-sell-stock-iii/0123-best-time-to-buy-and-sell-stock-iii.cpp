945c9d9250ecec32aeefec23c7c2c3ac1d16ac3e
Ashutosh Maurya <183261112+aashutosh585@users.noreply.github.com>
2025-11-11 15:08:31 +0530

    Time: 473 ms (11.67%), Space: 223.9 MB (26.01%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<vector<int>>>dp;
+    int solve(int i,vector<int>&arr,bool f,int use){
+        if(i==arr.size()) return 0;
+        if(use==0) return 0;
+        if(dp[i][f][use]!=-1) return dp[i][f][use];
+
+        int t;
+        if(f==0){
+            t= -arr[i]+solve(i+1,arr,1,use);
+        }
+        else{
+            t=arr[i]+solve(i+1,arr,0,use-1);
+        }
+        int n=solve(i+1,arr,f,use);
+        return dp[i][f][use]= max(n,t);
+
+    }
+
+    int maxProfit(vector<int>& arr) {
+        int n=arr.size();
+        dp.assign(n, vector<vector<int>>(2, vector<int>(3, -1)));
+        return solve(0, arr,0,2);
+    }
+};
