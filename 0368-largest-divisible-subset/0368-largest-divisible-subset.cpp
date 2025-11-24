5c8b719ea6f99a26c9564e9059d078a0e4f4bc24
Ashutosh Maurya <183261112+aashutosh585@users.noreply.github.com>
2025-11-24 20:28:39 +0530

    Time: 7 ms (93.23%), Space: 12.6 MB (74.33%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -4,47 +4,59 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
 
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        vector<int>dp(n,1),prev(n,-1);
 
-        // Fill DP bottom-up
-        for (int i = n - 1; i >= 0; i--) {
-            for (int prev = n; prev >= 0; prev--) {
-
-                int take = 0;
-                if (prev == n || nums[i] % nums[prev] == 0) {
-                    take = 1 + dp[i + 1][i];
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]%nums[j]==0 && dp[i]<dp[j]+1){
+                    dp[i]=dp[j]+1;
+                    prev[i]=j;
                 }
-
-                int ntake = dp[i + 1][prev];
-                dp[i][prev] = max(take, ntake);
             }
         }
 
-        // Reconstruction
         vector<int> ans;
-        int i = 0, prev = n;
-
-        while (i < n) {
-            int take = 0;
-
-            // If divisible, compute take
-            if (prev == n || nums[i] % nums[prev] == 0) {
-                take = 1 + dp[i + 1][i];
-            }
-
-            int ntake = dp[i + 1][prev];
-
-            // TAKE only if it matches dp[i][prev]
-            if ((prev == n || nums[i] % nums[prev] == 0) 
-                && dp[i][prev] == take) {
-
-                ans.push_back(nums[i]);
-                prev = i;
-            }
-            // else skip
-
-            i++;
+        int idx = max_element(dp.begin(), dp.end()) - dp.begin();
+        for(int i=idx;i>=0;i=prev[i]){
+            ans.push_back(nums[i]);
         }
+
+
+        // vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        // for (int i = n - 1; i >= 0; i--) {
+        //     for (int prev = n; prev >= 0; prev--) {
+
+        //         int take = 0;
+        //         if (prev == n || nums[i] % nums[prev] == 0) {
+        //             take = 1 + dp[i + 1][i];
+        //         }
+
+        //         int ntake = dp[i + 1][prev];
+        //         dp[i][prev] = max(take, ntake);
+        //     }
+        // }
+
+        // int i = 0, prev = n;
+
+        // while (i < n) {
+        //     int take = 0;
+
+        //     if (prev == n || nums[i] % nums[prev] == 0) {
+        //         take = 1 + dp[i + 1][i];
+        //     }
+
+        //     int ntake = dp[i + 1][prev];
+
+        //     if ((prev == n || nums[i] % nums[prev] == 0) &&
+        //         dp[i][prev] == take) {
+
+        //         ans.push_back(nums[i]);
+        //         prev = i;
+        //     }
+
+        //     i++;
+        // }
 
         return ans;
     }
