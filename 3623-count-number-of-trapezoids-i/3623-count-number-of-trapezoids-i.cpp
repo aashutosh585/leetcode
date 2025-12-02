a426238af6006557c6dc56a22f281756160630d4
Ashutosh Maurya <183261112+aashutosh585@users.noreply.github.com>
2025-12-02 12:54:25 +0530

    Time: 159 ms (29.74%), Space: 215.5 MB (28.16%) - LeetHub

--- 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.cpp
+++ 3623-count-number-of-trapezoids-i/3623-count-number-of-trapezoids-i.cpp
@@ -6,30 +6,13 @@ public:
         unordered_map<int,int>mpp;
         for(auto it:arr) mpp[it[1]]++;
 
-        vector<int>a;
-        long long sum=0;
-        for(auto &it:mpp){
-            int n=it.second;
-            if(n>1){
-               long long val=1LL *n * (n-1) /2;
-               a.push_back(val%mod);
-               sum+=val;
-            }
-        }
-        int m=a.size();
-        if(m<2) return 0;
-
-        long long ans=0;
-        for(int i=0;i<m-1;i++){
-            sum-=a[i];
-            ans= (ans+(sum*a[i])%mod) %mod;
-        }
-        // for(int i=0;i<m;i++){
-        //     for(int j=i+1;j<m;j++){
-        //         ans = (ans + (a[i] * a[j]) % mod) % mod;
-        //     }
-        // }
+        long long ans = 0, sum = 0;
         
-        return (int)ans;
+        for(auto& [_, it] : mpp) {
+            long long val = (long long)it * (it - 1) / 2;
+            ans += val * sum;
+            sum += val;
+        }
+        return ans % mod;
     }
 };
