a495440f3349fc14a2f1d6a8c35681faf38edcfa
Ashutosh Maurya <183261112+aashutosh585@users.noreply.github.com>
2026-01-03 10:26:50 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.67%) - LeetHub

---
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mod = 1e9 + 7;
+
+    int numOfWays(int n){
+        long long same = 6, diff = 6; 
+        for(int i = 2; i <= n; i++) {
+            long long newSame = (same * 3 + diff * 2) % mod;
+            long long newDiff = (same * 2 + diff * 2) % mod;
+            same = newSame;
+            diff = newDiff;
+        }
+        return (same + diff) % mod;
+    }
+};
