7810a7f6f26a0730c99065e3d07a87e39658cc7a
Ashutosh Maurya <183261112+aashutosh585@users.noreply.github.com>
2026-05-19 09:19:38 +0530

    Time: 3 ms (38.92%), Space: 54.6 MB (58.81%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -2,10 +2,17 @@ class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         int res=INT_MAX;
-        set<int>st;
-        for(auto &x:nums1) st.insert(x);
-        for(auto &y:nums2) if(st.find(y)!=st.end()) res=min(res, y);
-        
+        int i=0,j=0, n=nums1.size(), m= nums2.size();
+
+        while(i<n && j<m){
+            if(nums1[i]==nums2[j]){
+                res=min(res, nums1[i]);
+                i++,j++;
+            }
+            else if(nums1[i]>nums2[j]) j++;
+            else i++;
+        }
+
         return res==INT_MAX? -1: res;
   
     }
